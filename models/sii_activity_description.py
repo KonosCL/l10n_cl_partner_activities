df07c10c0dd149e9626b2f8f7c3b6663c510c6ff
Nelson Ramírez Sánchez <info@nelsonramirez.com>
2018-01-15 19:39:02 -0300

    Update from Original 9.0

--- models/sii_activity_description.py
+++ models/sii_activity_description.py
@@ -1,29 +1,33 @@
 # -*- coding: utf-8 -*-
+
 from odoo import models, fields
 
+class partner_activities(models.Model):
 
-class PartnerActivities(models.Model):
     _description = 'SII Economical Activities Printable Description'
     _name = 'sii.activity.description'
 
     name = fields.Char('Glosa', required=True, translate=True)
+
     vat_affected = fields.Selection(
-        [('SI', 'Si'), ('NO', 'No'), ('ND', 'ND')], 'VAT Affected',
+        (('SI', 'Si'), ('NO', 'No'), ('ND', 'ND')), 'VAT Affected',
         required=True, translate=True, default='SI')
+
     active = fields.Boolean(
-        'Active', help="Allows you to hide the activity without removing it.",
-        default=True)
+        'Active', help="Allows you to hide the activity without removing it.")
 
+    _defaults = {
+        'active': 1,
+    }
 
-class PartnerTurns(models.Model):
+class partner_turns(models.Model):
     _inherit = 'res.partner'
 
     activity_description = fields.Many2one(
         'sii.activity.description',
         string='Glosa Giro', ondelete="restrict")
 
-
-class CompanyTurns(models.Model):
+class company_turns(models.Model):
     _inherit = 'res.company'
 
     activity_description = fields.Many2one(
